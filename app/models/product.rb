5be8d2342dcdff3e7503240faf626f6cda66cc72
zmingshi <zmingshi@gmail.com>
2014-12-21 17:14:31 +0800

    Depot d : format price, test layout using functional tests and perform caching which seems not work.

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,7 @@ class Product < ActiveRecord::Base
 		with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a url for GIF, JPG or PNG image.'
 	}
+	def self.latest
+	  Product.order(:updated_at).last
+	end
 end
